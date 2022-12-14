b364cb77606fd58694043ab68a6696eb88b3aa83
Daniel EZS <dezoldan@hotmail.com>
2022-12-14 12:23:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace stringsImutavel.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Olá, ";
+            string s2 = "Daniel";            
+            s1 += s2;
+            Console.Write(s1);
+        }
+    }
+}
