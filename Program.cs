4c1879703089cc8f99203b658b0cb5927ec478b2
Daniel EZS <dezoldan@hotmail.com>
2022-12-14 13:14:29 -0300

    Atualização.

--- Program.cs
+++ Program.cs
@@ -5,13 +5,29 @@
         static void Main(string[] args)
         {
             string s1 = "Olá, ";
+            Console.Write($"O conteúdo de s1 é: \n");
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Console.WriteLine($"s[{i}] = '{s1[i]}' ('\\u{(int)s1[i]:x4}')");
+            }
             string s2 = "Daniel";
             string s3 = s1;
+            Console.Write($"O conteúdo de s3 é: \n");
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Console.WriteLine($"s[{i}] = '{s1[i]}' ('\\u{(int)s1[i]:x4}')");
+            }
             s1 += s2;
-            // s1 agora contém o novo objeto: Olá, Daniel, mas s3 é uma referência para a posição
-            // original na memória, que contém Olá,.
-            Console.Write($"{s3}");            
-            // Saída: Olá,
+            Console.Write("O conteúdo de s1 agora é: \n");
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Console.WriteLine($"s[{i}] = '{s1[i]}' ('\\u{(int)s1[i]:x4}')");
+            }
+            Console.Write("O conteúdo de s3 é: \n");
+            for (int i = 0; i < s3.Length; i++)
+            {
+                Console.WriteLine($"s[{i}] = '{s3[i]}' ('\\u{(int)s3[i]:x4}')");
+            }
         }
     }
 }
