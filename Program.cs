153951685fa3d3acc10ee41a4309db8edad4fc2d
Daniel EZS <dezoldan@hotmail.com>
2022-12-14 12:41:23 -0300

    Atualização.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@
         static void Main(string[] args)
         {
             string s1 = "Olá, ";
-            string s2 = "Daniel";            
+            string s2 = "Daniel";
+            string s3 = s1;
             s1 += s2;
-            Console.Write(s1);
+            // s1 agora contém o novo objeto: Olá, Daniel, mas s3 é uma referência para a posição
+            // original na memória, que contém Olá,.
+            Console.Write($"{s3}");            
+            // Saída: Olá,
         }
     }
 }
